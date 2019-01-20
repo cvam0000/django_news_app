9b62d26da4c089fe65596fb35446e506b86e676d
cvam0000 <cvam0000@gmail.com>
2019-01-20 09:33:06 +0530

    weather-app

--- news_app/urls.py
+++ news_app/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('search_engine.urls')),
+    path('abc', include('search_engine.urls')),
 ]
 
 
--- search_engine/views.py
+++ search_engine/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 
 def search(request):
-    render(request,'/search_engine/index.html')
+    template = loader.get_template('search_engine/index.html')
+    #template'search_engine/index.html')
+    context = {
+        
+    }
+    return HttpResponse(template.render(context, request))
