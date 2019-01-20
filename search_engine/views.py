6b11f5b2c269755211470c50e8cdaaf364da2f9f
cvam0000 <cvam0000@gmail.com>
2019-01-20 07:37:30 +0530

    index

--- search_engine/views.py
+++ search_engine/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def search(request):
-    
+    render(request,'/search_engine/index.html')
